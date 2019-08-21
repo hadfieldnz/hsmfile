a583d0aba2544802ab2037dc12cbee4336a3ea05
Mark Hadfield <mark.hadfield@niwa.co.nz>
2019-08-22 11:23:02 +1200

    A tweak to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(
    name='hsmfile',
-   version='0.0',
+   version='0.1',
+   license='MIT',
    description='Expedite access to files on a slow network volume',
    author='Mark Hadfield',
    author_email='mark.hadfield@niwa.co.nz',
