e95e0d7cff13f5efbcaf19278c249802a767fdbd
Mark Hadfield <mark.hadfield@niwa.co.nz>
2019-09-02 16:42:40 +1200

    Adopted the conventional package structure

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
    name='hsmfile',
-   version='0.1.1',
+   version='0.1.2',
    license='MIT',
    description='Expedite access to files on a slow network volume',
    long_description=long_description,
    author='Mark Hadfield',
    author_email='mark.hadfield@niwa.co.nz',
    url='https://github.com/hadfieldnz/hsmfile',
-   packages=['hsmfile'],
-   package_dir={'hsmfile':'.'},
+   packages=find_packages(),
    classifiers=[
        "Programming Language :: Python :: 3",
        "License :: OSI Approved :: MIT License",
